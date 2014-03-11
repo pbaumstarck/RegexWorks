7f93b0d397f9b9ba10772ce77c78fdd2a21a3c6c
Paul Baumstarck <pbaumstarck@gmail.com>
2014-03-11 08:54:13 -0700

    Added a redirect to the custom domain.

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
 
   def get(self):
+    if self.request.host.endswith("appspot.com"):
+      return self.redirect("http://regexworks.itsagoldenage.com")
     path = os.path.join(os.path.dirname(__file__), "index.html")
     self.response.out.write(open(path, "r").read())
 
